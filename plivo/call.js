8ed205cf92263c16a93930c0a4528d4cf4157a75
Chris Grimmett <chris@grimtech.net>
2016-08-29 11:17:03 -0700

    plivo sms test

--- plivo/call.js
+++ plivo/call.js
@@ -10,50 +10,17 @@ var path = require('path');
 app.use(express.static('dist'));
 
 
-/**
- * a call is received.
- *
- *   - announce app name and ask for an ID number.
- *   - if user enters digits, forward user to new endpoint
- *   - hangup if no digits received
- *
- */
-app.post('/ld36/marshall.smith/call/', function(req, res) {
-    //console.log(req.params);
-    //var hangup = req.params['HangupCause'];
-    //console.log('hangup: ' + hangup);
+/** SMS received **/
+app.post('/ld36/sms/', function(req, res) {
+    console.log(req.body);
+    throw 'thats what u get';
     var r = plivo.Response();
-
-    // The digits pressed are send to this endpoint but because of
-    // redirect: true, this endpoint is redirected to for further
-    // processing and returning of XML instructions
-    var digitOptions = {
-        action: url.resolve(rootURL, '/response/sip/digits/'),
-        redirect: "true",
-        timeout: 5,     // first digit must be received within 3s
-        digitTimeout: 2 // time between digits must not be greater than 2s
+    var smsOptions = {
+      message: "Network Engineers Frontier SMS Gateway. Reply with your party's extension.",
+      src: "15092625208",
+      dst: ""
     };
-
-    //r.addRecord(recordOptions);
-    //r.addWait({length: "9"});
-    var d = r.addGetDigits(digitOptions);
-    d.addSpeak('enter your designator ID number');
-    //d.addWait({length: "5"});
-    r.addSpeak('end due to inaction');
-    r.addHangup({"reason": "no designator ID number entered"});
-    // var dial = r.addDial();
-    // dial.addNumber('15099220951');
-    //r.addWait({ length: "3" });
-
-    // if (hangup) {
-    //     res.end("SIP Route hangup callback");
-    //     return;
-    // }
-
-    res.set({
-        'Content-Type': 'text/xml'
-    });
-    res.end(r.toXML());
+    r.addMessage()
 
 });
 
