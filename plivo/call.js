195234d1cc4657917b40e8e9ac809fa1f430bf3f
Chris Grimmett <chris@grimtech.net>
2016-08-30 13:52:59 -0700

    switch object form

--- plivo/call.js
+++ plivo/call.js
@@ -22,34 +22,35 @@ app.post('/ld36/sms', function(req, res) {
 
     // Pete's Ext
     if (senderMessage == 'A382' || senderMessage == 'a382') {
-      smsOptions.text = "x843p_33.32.12.txt.hex";
+      smsOptions['text'] = "x843p_33.32.12.txt.hex";
     }
 
     // Sara's Ext
     else if (senderMessage == 'DV47' || senderMessage == 'dv47') {
-      smsOptions.text = 'x666p_66.66.66.txt.hex';
+      smsOptions['text'] = 'x666p_66.66.66.txt.hex';
     }
 
     // Marshall's Ext
     else if (senderMessage == '43E0' || senderMessage == '43e0') {
-      smsOptions.text = 'x448p_21.21.21.txt.hex';
+      smsOptions['text'] = 'x448p_21.21.21.txt.hex';
     }
 
     // Dr. Destin's Ext
     else if (senderMessage == '88Z2' || senderMessage == '88z2') {
-      smsOptions.text = 'x903p_48.83.52.txt.hex';
+      smsOptions['text'] = 'x903p_48.83.52.txt.hex';
     }
 
 
     else {
-      smsOptions.text = "Network Engineers Frontier SMS Gateway. Reply with your extension."
+      smsOptions['text'] = "Network Engineers Frontier SMS Gateway. Reply with your extension."
     }
 
-    smsOptions.src = 15092625208;
-    smsOptions.dst = sender;
-    console.log(r.toXML());
+    smsOptions['src'] = 15092625208;
+    smsOptions['dst'] = sender;
 
     r.addMessage(smsOptions);
+    console.log(r.toXML());
+
     res.set({
         'Content-Type': 'text/xml'
     });
